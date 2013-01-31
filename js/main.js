32a1268db3372fa79e129a1fd3d72ac4bf8f6f11
Andrew Kennedy <akennedy2292@gmail.com>
2013-01-31 16:40:31 -0500

    more content

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".portfolio").mousemove(function(e) {
+		var parentOffset = $(this).parent().offset(); 
+		var relX = e.pageX - parentOffset.left;
+		if ( $(".portfolio-container").children("img").length > 4 ) {
+			$(".portfolio-container").css({marginLeft : -relX + 100 });
+		}
+	});
+});
