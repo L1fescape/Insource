a95121197c269bb1c823c13a152003699cf841a1
Andrew Kennedy <akennedy2292@gmail.com>
2013-01-31 14:43:12 -0500

    new layout lookin fresh/

--- js/bindings.js
+++
@@ -1,9 +0,0 @@
-$("body").on({
-	"click" : function() {
-		$(".portfolio-thumb-container img").removeClass("active");
-		$(this).addClass("active");
-		var index = $(".portfolio-thumb-container img").index(this);
-		var left = index * $(".portfolio").width() * -1;
-		$(".portfolio-container").animate({"marginLeft" : left}, 300);
-	}
-}, ".portfolio-thumb-container img");
