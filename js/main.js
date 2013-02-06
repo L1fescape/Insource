f3d00b8d2a605dd69963765c7d3053be737bb7cc
Andrew Kennedy <akennedy2292@gmail.com>
2013-02-06 18:58:05 -0500

    adding more people

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(".portfolio").mousemove(function(e) {
-		var parentOffset = $(this).parent().offset(); 
-		var relX = e.pageX - parentOffset.left;
-		if ( $(".portfolio-container").children("img").length > 4 ) {
-			$(".portfolio-container").css({marginLeft : -relX + 100 });
-		}
-	});
-});
