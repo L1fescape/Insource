d8b50b7a27e5530e0611c37feefbdaf830e8118e
Andrew Kennedy <akennedy2292@gmail.com>
2013-02-06 20:53:28 -0500

    added parra

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	$(".portfolio").mousemove(function(e) {
 		var parentOffset = $(this).parent().offset(); 
 		var relX = e.pageX - parentOffset.left;
-		if ( $(".portfolio-container").children("img").length > 4 ) {
+		if ( $(".portfolio-container").children("a").length > 4 ) {
 			$(".portfolio-container").css({marginLeft : -relX + 100 });
 		}
 	});
-	$('.portfolio a').colorbox({rel:'gal'});
+	$('.portfolio a').colorbox({rel:'gal', height:'100%'});
 });
